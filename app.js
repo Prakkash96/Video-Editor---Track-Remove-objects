c99c1ddceae4c1f435e05cc148952c39fa762086
Prakkash <72903400+Prakkash96@users.noreply.github.com>
2024-01-13 20:05:33 +0530

    Update index.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ffmpegConcat = require('ffmpeg-concat');
 const app = express();
 app.use(express.json());
 const greenScreenPOC = require('./logics/greenScreenPOC');
@@ -16,14 +17,14 @@ async function replaceGreenScreenWithtextedImage() {
 }
 
 
-async function removeObjInAVideo() {
+async function removeStaticObject() {
     app.use(express.static(__dirname));
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     });
     app.post('/button-click', async (req, res) => {
         try {
-            await removeObjPOC.eraseObject(req.body);
+            await removeObjPOC.eraseStaticObject(req.body);
             console.log('DONE');
             const responseMessage = 'Video generated successfully';
             res.json({ message: responseMessage });
@@ -40,12 +41,50 @@ async function removeObjInAVideo() {
     return;
 }
 
+async function objectTracking() {
+    app.use(express.static(__dirname));
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
+    });
+    app.post('/button-click', async (req, res) => {
+        try {
+            await removeObjPOC.trackObject(req.body);
+            console.log('DONE');
+            const responseMessage = 'Video generated successfully';
+            res.json({ message: responseMessage });
+        } catch (error) {
+            console.error('An error occurred:', error);
+            res.status(500).send('Error occurred while erasing object');
+        }
+    });
+
+    app.listen(5100, () => {
+        console.log('Server started on port 5100');
+    });
+    return;
+}
+
+async function glTransition() {
+    await ffmpegConcat({
+        output: 'output.mp4',
+        videos: [
+            'f.mp4',
+            'g.mp4'
+        ],
+        transition: {
+            name: 'squeeze',
+            duration: 1000
+        }
+    });
+    return true;
+}
 
 async function main() {
     console.log("START");
     // await replaceGreenScreenWithtextedImage();
-    await removeObjInAVideo();
-
+    // await removeStaticObject();
+    await objectTracking();
+    // await glTransition();
 }
 
 main();
